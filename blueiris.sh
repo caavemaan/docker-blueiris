0a969da1f2fd9719a61a69c05ad1c220a8f6833c
caavemaan <mark422e@gmail.com>
2022-06-22 08:34:18 -0500

    Update blueiris.sh

--- blueiris.sh
+++ blueiris.sh
@@ -2,7 +2,7 @@
 
 BLUEIRIS_EXE="/root/prefix32/drive_c/Program Files/Blue Iris 4/BlueIris.exe"
 PREFIX_DIR="/root/prefix32"
-INSTALL_EXE="/root/BlueIris_48603.exe"
+INSTALL_EXE="/root/blueiris.exe"
 
 if [ ! -d "$PREFIX_DIR/drive_c" ]; then
     mv /root/prefix32_original/* /root/prefix32
@@ -14,8 +14,8 @@ if [ ! -e "$BLUEIRIS_EXE" ] ; then
     if [ ! -e "$INSTALL_EXE" ] ; then
         wget https://blueirissoftware.com/BlueIris_48603.exe
     fi
-    wine BlueIris_48603.exe
-    rm BlueIris_48603.exe
+    wine blueiris.exe
+    rm blueiris.exe
 fi
 
 wine "$BLUEIRIS_EXE"
